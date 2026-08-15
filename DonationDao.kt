ab1efd57498ac833b7ac22b663cdbebfd14cc685
vutomii <VutomiKhumalo659@gmail.com>
2026-08-15 13:51:34 +0200

    Delete DonationDao.kt

--- DonationDao.kt
+++
@@ -1,8 +0,0 @@
-package com.maretlagadi.welfarecentre.ui.volunteer
-
-import com.maretlagadi.welfarecentre.data.entities.Event
-import com.maretlagadi.welfarecentre.data.entities.ShiftStatus
-import com.maretlagadi.welfarecentre.data.entities.VolunteerShift
-
-/** A shift paired with its event details, for display on the Volunteer dashboard. */
-data class ShiftUi(val shift: VolunteerShift, val event: Event?)
