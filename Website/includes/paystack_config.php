bfaf6e074e7ae2b660000ebf415d95dde3a641e4
Prince Mabhena <princemabena182@gmail.com>
2026-09-03 18:01:18 +0200

    Delete Website directory

--- Website/includes/paystack_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Paystack TEST keys - replace with your own from the Paystack dashboard
-// Add this to a .gitinore, so our secret key won't be seen in public
-
-define('PAYSTACK_PUBLIC_KEY', 'pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
-define('PAYSTACK_SECRET_KEY', 'sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');?>
